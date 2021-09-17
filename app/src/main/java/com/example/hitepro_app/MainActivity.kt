b96118a3eaa39a333a8504cdb9faaf2030a2ddb5
Deniz Kek <88910524+Den1z1@users.noreply.github.com>
2021-09-17 04:46:02 +0300

    Bottom nav bar

--- app/src/main/java/com/example/hitepro_app/MainActivity.kt
+++ app/src/main/java/com/example/hitepro_app/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.hitepro_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val bottomNavigationView = findViewById<BottomNavigationView
+                >(R.id.navBottom)
+        val navController = findNavController(R.id.navHostFragment)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
